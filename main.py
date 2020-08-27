4e5fc003a48e055bc4b835f585a4cec61ed81f4d
aeikay <67679263+aeikay@users.noreply.github.com>
2020-08-27 13:58:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+# CGPA calculator sample program for ECE Students #
+# Enter the number of semesters completed and give the sgpa of corresponding semesters #
+# CGPA will be outputted #
+
 num = int(input("Enter the number of semesters completed:"))
 i = 0
 arr = []
 for i in range(num):
     arr.append(input("Enter your sgpa of " + str(i+1) + "th" + " sem : "))
 
-    def cgpaCalculator(arr):
-        credits = [17,17,21,22,23,22,22,22]
-        creditTotal = 0
-        summ = 0
-        for i in range(num):
-                  summ += float(credits[i]* float(arr[i]))
-                  creditTotal += credits[i]
+def cgpaCalculator(arr):
+    credits = [17,17,21,22,23,22,22,22]
+    creditTotal = 0
+    summ = 0
+    for i in range(num):
+             summ += float(credits[i]* float(arr[i]))
+             creditTotal += credits[i]
 
-        cgpa = summ/creditTotal
-        print("{:.2f}".format(cgpa))
+    cgpa = summ/creditTotal
+    print("{:.2f}".format(cgpa))
 
 cgpaCalculator(arr)
 
